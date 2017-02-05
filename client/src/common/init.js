ce3bc5916f255e0399a37af9ca22588666e87578
Ryou aki <46517115@qq.com>
2017-02-05 12:34:08 +0800

    update

--- client/src/common/init.js
+++ client/src/common/init.js
@@ -1,1 +1,30 @@
-console.log('init')
+((root) => {
+    
+    // Get server address.
+    let serverUrl = document.getElementsByTagName('meta')['server'];
+
+    let initRootFontSize = () => {
+        let rootFontSize = 0;
+        let width = document.body.clientWidth;
+        let html = document.getElementsByTagName('html')[0];
+        if (width < 1000) {
+            rootFontSize = width / 10;
+        } else {
+            rootFontSize = 14;
+        }
+        html.style.fontSize = rootFontSize + 'px';
+    }
+
+    let resizeCallback = () => {
+        initRootFontSize();
+    }
+
+    window.addEventListener('resize', resizeCallback, false);
+
+    root.hi1896 = {
+        initRootFontSize
+    }
+
+    initRootFontSize();
+
+})(window||global);
